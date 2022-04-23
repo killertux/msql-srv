fdfbcbe6530c46d8741133fec1145bdb6e12a64c
Clemente <bruno.canguru@hotmail.com>
2022-04-23 14:21:29 -0300

    Add fake Auth Plugin

--- src/lib.rs
+++ src/lib.rs
@@ -272,6 +272,7 @@ impl<B: MysqlShim<RW>, RW: Read + Write> MysqlIntermediary<B, RW> {
         self.rw.write_all(&[0x08, 0x00, 0x00, 0x00])?; // TODO: connection ID
         self.rw.write_all(&b";X,po_k}\0"[..])?; // auth seed
         let capabilities = &mut [0x00, 0x42]; // 4.1 proto
+        capabilities[1] |= 0x80; // Secure connection
         #[cfg(feature = "tls")]
         if tls_conf.is_some() {
             capabilities[1] |= 0x08; // SSL support flag
@@ -279,11 +280,12 @@ impl<B: MysqlShim<RW>, RW: Read + Write> MysqlIntermediary<B, RW> {
         self.rw.write_all(capabilities)?;
         self.rw.write_all(&[0x21])?; // UTF8_GENERAL_CI
         self.rw.write_all(&[0x00, 0x00])?; // status flags
-        self.rw.write_all(&[0x00, 0x00])?; // extended capabilities
+        self.rw.write_all(&[0x08, 0x00])?; // extended capabilities
         self.rw.write_all(&[0x00])?; // no plugins
         self.rw.write_all(&[0x00; 6][..])?; // filler
         self.rw.write_all(&[0x00; 4][..])?; // filler
         self.rw.write_all(&b">o6^Wz!/kM}N\0"[..])?; // 4.1+ servers must extend salt
+        self.rw.write_all(&b"mysql_native_password\0"[..])?; // Auth Plugin name
         self.rw.flush()?;
 
         let mut auth_context = AuthenticationContext::default();
